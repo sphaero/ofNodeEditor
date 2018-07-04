06b716dbd089c4eb98df81a4513f9e117850dfb0
Arnaud Loonstra <arnaud@sphaero.org>
2018-07-04 16:35:46 +0200

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,104 @@
+#include "ofApp.h"
+#include "Nodes.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetLogLevel(OF_LOG_VERBOSE);
+
+    //required call
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.MouseDrawCursor = false;
+    io.Fonts->AddFontDefault();
+    io.Fonts->AddFontFromFileTTF("/home/arnaud/src/imgui/misc/fonts/Roboto-Medium.ttf", 16.0f);
+    gui.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+}
+
+void ofApp::doGui() {
+
+    gui.begin();
+    // Create a main menu bar
+    float mainmenu_height = 0;
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::MenuItem("Open ...", "Ctrl+O")) {  }
+            if (ImGui::MenuItem("Save ...", "Ctrl+S"))   { }
+            if (ImGui::MenuItem("Exit", "Ctrl+W"))  { ofExit(0); }
+            ImGui::EndMenu();
+        }
+        mainmenu_height = ImGui::GetWindowSize().y;
+        ImGui::EndMainMenuBar();
+    }
+    ImGui::SetNextWindowPos(ImVec2( 0, mainmenu_height ));
+    ImGui::SetNextWindowSize(ImVec2( ofGetWidth()-351, ofGetHeight()-mainmenu_height));
+    ImGui::Begin("clientspanel", NULL,  ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_NoBringToFrontOnFocus);
+    nodes.ProcessNodes();
+    ImGui::End();
+    gui.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    doGui();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxImGui.h"
+#include "Nodes.h"
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void doGui();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    ofxImGui::Gui gui;
+    ImGui::Nodes nodes;
+};
